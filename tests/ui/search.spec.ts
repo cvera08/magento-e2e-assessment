44672d8006332e6547ce12b629b1284bf705c663
Carlos Vera <cvera08@gmail.com>
2025-04-15 17:47:45 -0300

    🧪 test(case-1): initial version of search test recorded with codegen (to be refactored)

--- tests/ui/search.spec.ts
+++ tests/ui/search.spec.ts
@@ -1,5 +1,14 @@
-import {test, expect} from '@playwright/test'
+import { test, expect } from '@playwright/test'
 
 test('Test Case 1 - search android', async ({ page }) => {
     await page.goto('/');
+
+    // TODO [cvera]:: Codegen selectors – replace with reusable Page Object methods
+    //await page.goto('https://start.duckduckgo.com/');
+    await page.getByRole('combobox', { name: 'Search with DuckDuckGo' }).click();
+    await page.getByRole('combobox', { name: 'Search with DuckDuckGo' }).fill('android');
+    //await page.getByRole('combobox', { name: 'Search with DuckDuckGo' }).press('Enter');
+    await page.getByRole('button', { name: 'Search', exact: true }).click();
+    await page.getByRole('link', { name: 'Android | Do More With Google' }).click();
+    //await page.goto('https://duckduckgo.com/?t=h_&hps=1&start=1&q=android&ia=web');
 });
