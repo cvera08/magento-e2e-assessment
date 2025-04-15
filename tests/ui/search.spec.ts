19321fb66a608789284cc91c98e7626f7329a1cc
Carlos Vera <cvera08@gmail.com>
2025-04-15 18:00:38 -0300

    ♻️ refactor(case-1): clean up codegen output and remove unused lines

--- tests/ui/search.spec.ts
+++ tests/ui/search.spec.ts
@@ -3,12 +3,9 @@ import { test, expect } from '@playwright/test'
 test('Test Case 1 - search android', async ({ page }) => {
     await page.goto('/');
 
-    // TODO [cvera]:: Codegen selectors – replace with reusable Page Object methods
-    //await page.goto('https://start.duckduckgo.com/');
+    // TODO [cvera]: Codegen selectors – replace with reusable Page Object methods
     await page.getByRole('combobox', { name: 'Search with DuckDuckGo' }).click();
     await page.getByRole('combobox', { name: 'Search with DuckDuckGo' }).fill('android');
-    //await page.getByRole('combobox', { name: 'Search with DuckDuckGo' }).press('Enter');
     await page.getByRole('button', { name: 'Search', exact: true }).click();
     await page.getByRole('link', { name: 'Android | Do More With Google' }).click();
-    //await page.goto('https://duckduckgo.com/?t=h_&hps=1&start=1&q=android&ia=web');
 });
