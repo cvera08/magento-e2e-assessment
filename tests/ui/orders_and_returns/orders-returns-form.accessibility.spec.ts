ff4c816f272935dc429aee5dd69a9cbb055d5ed4
Carlos Vera <cvera08@gmail.com>
2025-04-17 12:06:37 -0300

    🧪 feat: implement first accessibility test for Orders and Returns form

--- tests/ui/orders_and_returns/orders-returns-form.accessibility.spec.ts
+++ tests/ui/orders_and_returns/orders-returns-form.accessibility.spec.ts
@@ -1,1 +1,15 @@
-// TODO [cvera]: Implement test to verify that the Orders and Returns form is accessible
+import { test, expect } from '@playwright/test';
+import * as ordersReturnsFormPage from '../../../modules/orders-returns-form.page';
+
+/**
+ * Orders and Returns Form — accessibility validation.
+ */
+test('Test Case 1 - verify form is accessible', async ({ page }) => {
+    await page.goto('/sales/guest/form/', { waitUntil: 'load' }); // 🐞 Ensures full page load before interacting
+
+    const form = await ordersReturnsFormPage.ordersReturnsForm(page);
+    await expect(form).toBeVisible(); // Verify form visibility
+
+    const emailField = await ordersReturnsFormPage.emailInput(page);
+    await expect(emailField).toBeVisible(); // Optional. Ensure required fields, like email, is visible and accessible
+});
